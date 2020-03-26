3dca42a9719edf1b89d3f7118b644a1b88cc954c
Student_MA122 <student_ma122@Student-MA122.fios-router.home>
2020-03-25 20:52:15 -0400

    colors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+  this.state =  {
+    color: 'yellow',
+    clickCounter: 0 
+  }  
 }
 
-export default App;
+onChange = (e) => {
+  let i = Math.floor(Math.random()*10);
+  let colors = ['red','blue','green', 'purple','wheat','orange', 'black', 'yellow', 'brown', 'lightblue'];
+  this.setState({
+    color: colors[i],
+    clickCounter: this.state.clickCounter++
+  })
+}
+  render(){
+    return (
+      <div className='App'>
+          <h1>Box color change</h1>
+          <div className="colorBox"
+          style = {{backgroundColor:this.state.color}} onClick={this.onChange} >
+            <p >Click me </p>
+          </div>
+      </div>
+    )
+  }
+}
+
